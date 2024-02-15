55066502b9c45772cda98ddfc4fa13fb662210bc
Alison9Saim <alisaimwindows@gmail.com>
2024-02-15 13:50:31 -0500

    setup git

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+
 require('dotenv').config()
 var axios = require('axios');
 var bodyParser = require('body-parser');
 const {MongoClient, ObjectId} = require("mongodb");
 //var mongoose = require('mongoose');
 var app = express();
+
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,16 +19,11 @@ var score = 0;
 var currentIndex = 0;
 
 var questions = [];
-console.log("Start of script");
-console.log(process.env.MONGO_USERNAME);
+
 
 const uri = "mongodb+srv://"+process.env.MONGO_USERNAME+":"+process.env.MONGO_PASSWORD+"@cluster0.j2vmca7.mongodb.net/";
-
-
 //const uri  = "mongodb+srv://alison:o65Xnxr8atd6Uqka@cluster0.j2vmca7.mongodb.net/";
 const client = new MongoClient(uri);
-
-process.env.S3_BUCKET
 
 
 app.get('/test', (req, res) => {
@@ -35,21 +33,15 @@ app.get('/test', (req, res) => {
 
 app.get('/', async (req, res) => {
 
-
     async function main() {
-
+        score = 0;
         questions = [];
         currentIndex = 0;
 
     
         try {
             await client.connect();
-            // Make the appropriate DB calls
-            //await  listDatabases(client);
-
-
             async function getQuestions(client){
-                //const result = await client.db("sample_questions").collection("questions").find().toArray();
                 const result = await client.db("sample_questions").collection("questions").aggregate([{ $sample: { size: 5 } }]).toArray();
                 questions = result;
                 currentIndex = 0;
@@ -58,12 +50,6 @@ app.get('/', async (req, res) => {
     
             await getQuestions(client);
 
-
-
-
-
-
-
             res.render("index", {questions, something: score, q: currentIndex})
         }
         catch (e) {console.error(e);}
@@ -71,14 +57,12 @@ app.get('/', async (req, res) => {
     }
 
     await main().catch(console.error);
-
-
     
 });
 
 
 
-app.get('/gameover', (req, res) => {
+app.get('/gameover', async (req, res) => {
     res.render('gameover', {questions, something: score});
 });
 
@@ -107,103 +91,26 @@ app.post('/', async (req, res) => {
 
                 try {
                     await client.connect();
-                    //await  updateQues(client, "Red or Orange? Which is more popular colour?");
-                    //await updateQues(client, new ObjectId('65ab3131ca697074795b81ca'));
-
-
                     async function updateListingByName_A(client, nameOfListing) {
                         const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {a_vote: currentItem.a_vote} });
                         console.log(`${result.matchedCount} document(s) matched the query criteria.`);console.log(`${result.modifiedCount} document(s) was/were updated.`);
                     }
-
-
-
-
                     await updateListingByName_A(client, questions[currentIndex]._id);
-
-
 
                     debugger;
                     if(currentIndex == 4 || currentIndex == 5 ){
-
                         async function getQuestionsAgain(client){
                             const result = await client.db("sample_questions").collection("questions").aggregate([{ $sample: { size: 5 } }]).toArray();
                             questions = result;
                             currentIndex = 0;
                             debugger;
                             console.log("new questions are ready");
-                            
-                            //res.render("index", {questions, something: score, q: currentIndex})
-                            //res.send({questions, something: score, q: currentIndex});
                         }
-        
                          await getQuestionsAgain(client);
-
                     }
 
                     currentIndex = currentIndex + 1;
                     res.render("index", {questions, something: score, q: currentIndex});
-
-
-
-
-                    /*
-                    if(currentIndex == 4 || currentIndex == 5 ){
-                        debugger;
-                        //CHECK TO COUNTER
-                        questions = questions;
-                
-                        async function getNewQuestionsAgain(){
-                            try {
-                                await client.connect();
-                
-                                console.log("A or B was clicked, should we get new questions");
-                                console.log("________________________________New questions")
-                
-                                async function getQuestionsAgain(client){
-                                    const result = await client.db("sample_questions").collection("questions").aggregate([{ $sample: { size: 1 } }]).toArray();
-                                    questions = result;
-                                    currentIndex = 0;
-                                    debugger;
-                                    console.log("new questions are ready");
-                                    
-                                    //res.render("index", {questions, something: score, q: currentIndex})
-                                    //res.send({questions, something: score, q: currentIndex});
-                                }
-                
-                                 await getQuestionsAgain(client);
-                                 
-                
-                
-                
-                
-                
-                            }
-                            catch (e) {console.error(e);}
-                            finally {console.log("connect B is finally closed");await client.close();}
-                
-                
-                        }
-                
-                        await getNewQuestionsAgain().catch(console.error);
-                        //res.render("index", {questions, something: score, q: currentIndex})
-                        
-                    }
-                    */
-
-
-
-
-
-
-
-
-                    
-                    //if(currentIndex != 4){
-                    //    currentIndex = currentIndex + 1;
-                        //res.render("index", {questions, something: score, q: currentIndex});
-                    //}
-
 
 
 
@@ -212,40 +119,38 @@ app.post('/', async (req, res) => {
                 } finally {
                     console.log("connect C is finally closed");
                     await client.close();
-                    //currentIndex = currentIndex + 1;
                 }
             }
-
-
-
-
             await updateOneDocByID().catch(console.error);
 
             
             
             
-            //return;
+
         }else{
             console.log("WRONG!");
-
-            currentItem.b_vote = currentItem.b_vote + 1;
+            currentItem.a_vote = currentItem.a_vote + 1;
 
             async function updateOneDocByID_B_Wrong() {
-
                 try {
                     await client.connect();
-
                     async function updateListingByName_B_1(client, nameOfListing) {
-                        const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {b_vote: currentItem.b_vote} });
+                        const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {a_vote: currentItem.a_vote} });
                         console.log(`${result.matchedCount} document(s) matched the query criteria.`);console.log(`${result.modifiedCount} document(s) was/were updated.`);
                     }
-
                     await updateListingByName_B_1(client, questions[currentIndex]._id);
 
 
+                    var randGameOverText = "";
+                    async function getRandGameOverMessage(client){
+                        const result = await client.db("sample_questions").collection("gameover_message").aggregate([{ $sample: { size: 1 } }]).toArray();
+                        rand_gameover_message = result;
+                    }
+            
+                    await getRandGameOverMessage(client);
 
                     res.setHeader('Content-Type', 'text/html');
-                    res.render('gameover', {something: score});
+                    res.render('gameover', {something: score, message: rand_gameover_message[0].message_text});
 
                 } catch (e) {
                     console.error(e);
@@ -254,7 +159,6 @@ app.post('/', async (req, res) => {
                     await client.close();
                 }
             }
-            
            await updateOneDocByID_B_Wrong().catch(console.error);
 
         }
@@ -274,18 +178,26 @@ app.post('/', async (req, res) => {
 
                 try {
                     await client.connect();
-                    //await  updateQues(client, "Red or Orange? Which is more popular colour?");
-                    //await updateQues(client, new ObjectId('65ab3131ca697074795b81ca'));
-
                     async function updateListingByName_B_2(client, nameOfListing) {
                         const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {b_vote: currentItem.b_vote} });
                         console.log(`${result.matchedCount} document(s) matched the query criteria.`);console.log(`${result.modifiedCount} document(s) was/were updated.`);
                     }
-
                     await updateListingByName_B_2(client, questions[currentIndex]._id);
+
+                    debugger;
+                    if(currentIndex == 4 || currentIndex == 5 ){
+                        async function getQuestionsAgain(client){
+                            const result = await client.db("sample_questions").collection("questions").aggregate([{ $sample: { size: 5 } }]).toArray();
+                            questions = result;
+                            currentIndex = 0;
+                            debugger;
+                            console.log("new questions are ready");
+                        }
+                         await getQuestionsAgain(client);
+                    }
+
                     currentIndex = currentIndex + 1;
-
-
+                    res.render("index", {questions, something: score, q: currentIndex});
 
                 } catch (e) {
                     console.error(e);
@@ -296,7 +208,6 @@ app.post('/', async (req, res) => {
             }
             
             await updateOneDocByID_B_Right().catch(console.error);
-            //currentIndex = currentIndex + 1;
 
 
 
@@ -304,24 +215,32 @@ app.post('/', async (req, res) => {
 
         }else{
             console.log("WRONG!");
-            currentItem.a_vote = currentItem.a_vote + 1;
+            currentItem.b_vote = currentItem.b_vote + 1;
 
             async function updateOneDocByID_B_adam() {
-
                 try {
                     await client.connect();
-
                     async function updateListingByName_B_c(client, nameOfListing) {
-                        const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {a_vote: currentItem.a_vote} });
+                        const result = await client.db("sample_questions").collection("questions").updateOne({ _id: nameOfListing }, { $set: {b_vote: currentItem.b_vote} });
                         console.log(`${result.matchedCount} document(s) matched the query criteria.`);console.log(`${result.modifiedCount} document(s) was/were updated.`);
                     }
-
                     await updateListingByName_B_c(client, questions[currentIndex]._id);
 
 
+                    var randGameOverText = "";
+                    async function getRandGameOverMessage(client){
+                        const result = await client.db("sample_questions").collection("gameover_message").aggregate([{ $sample: { size: 1 } }]).toArray();
+                        rand_gameover_message = result;
+                    }
+            
+                    await getRandGameOverMessage(client);
 
                     res.setHeader('Content-Type', 'text/html');
-                    res.render('gameover', {something: score});
+                    res.render('gameover', {something: score, message: rand_gameover_message[0].message_text});
+
+
+                    //res.setHeader('Content-Type', 'text/html');
+                    //res.render('gameover', {something: score});
 
                 } catch (e) {
                     console.error(e);
@@ -330,14 +249,8 @@ app.post('/', async (req, res) => {
                     await client.close();
                 }
             }
-            
             await updateOneDocByID_B_adam().catch(console.error);
-
-
         }
-
-
-
 
     }
 
