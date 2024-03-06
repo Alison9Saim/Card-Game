cda4f4de1586647e8ce5c59a82fb60bf8e56f349
alison9saim <alisaimwindows@gmail.com>
2024-03-06 18:06:57 -0500

    updated add question page

--- public/javascripts/clear_contact_form.js
+++ public/javascripts/clear_contact_form.js
@@ -1,8 +1,8 @@
 
 function clearform(){
-    $("#exampleFormControlInput1").val('');
-    $("#exampleFormControlTextarea1").val('');
-    $("#validationTooltip02").val('');
+    $("#emailAddress").val('');
+    $("#subjectLine").val('');
+    $("#emailBody").val('');
 };
 
 
